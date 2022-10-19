d690fe1f93bed5991094da926f69a8a2d3e60ea4
Aleksey <a_zhuravlyov@bk.ru>
2022-10-19 18:05:56 +0300

    fixed except_num number

--- Calculator/except_num.py
+++ Calculator/except_num.py
@@ -1,11 +1,9 @@
 def get_number():
     number = input('Enter a number: ')
     try:
-        float(number)
+        number = float(number)
         print('Its OK.')
         return number
     except ValueError:
         print('Its not a number. Try again.')
         return get_number()
-
-
--- Calculator/user_interface.py
+++ Calculator/user_interface.py
@@ -1,4 +1,9 @@
-import model_sum as sum
-import except as ex
+import model_sum
+import except_num as ex
+
 def user_interface():
-    print()
+    number_a = ex.get_number()
+    number_b = ex.get_number()
+    print(model_sum.sum(number_a,number_b))
+    
+user_interface()
