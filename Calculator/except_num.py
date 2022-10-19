0f90808f3dd6a8f356e68804bb4ad518a21ba709
Aleksey <a_zhuravlyov@bk.ru>
2022-10-19 22:40:13 +0300

    Added div zero

--- Calculator/except_num.py
+++ Calculator/except_num.py
@@ -7,3 +7,14 @@ def get_number():
     except ValueError:
         print('Its not a number. Try again.')
         return get_number()
+
+
+def zero_except():
+    number = input('Enter a number: ')
+    number = float(number)    
+    if number == 0:
+        print('Its a zero. Try again.')
+        return zero_except()
+    print('Its OK.')
+    return number
+zero_except()
