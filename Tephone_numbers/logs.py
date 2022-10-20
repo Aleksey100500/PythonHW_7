3a23d8ae38dc139fcf8d05f9954246505124fcf6
Aleksey <a_zhuravlyov@bk.ru>
2022-10-20 12:25:41 +0300

    Fixed logs tel

--- Tephone_numbers/logs.py
+++ Tephone_numbers/logs.py
@@ -1,16 +1,16 @@
-import datetime as time
+import datetime
 
 def logs_csv(surname, name, tel_number, params):
-    time_calc = time.now().strftime('%H:%M')
-    with open('log.csv', 'a') as log_file:
-        log_file.write('{}; {}; {}; {}; \n Time: {}'.format(surname, name, tel_number, params, time_calc))
+    time_calc = datetime.datetime.now()
+    with open('log_tel.csv', 'a') as log_file:
+        log_file.write('{}; {}; {}; {}; Time: {}\n'.format(surname, name, tel_number, params, time_calc))
 
 def logs_json(surname, name, tel_number, params):
-    time_calc = datetime.now().strftime('%H:%M')
-    with open('log.json', 'a') as log_file:
-        log_file.write('{}; {}; {}; {}; \n Time: {}'.format(surname, name, tel_number, params, time_calc))
+    time_calc = datetime.datetime.now()
+    with open('log_tel.json', 'a') as log_file:
+        log_file.write('{}; {}; {}; {}; Time: {}\n'.format(surname, name, tel_number, params, time_calc))
 
 def logs_xml(surname, name, tel_number, params):
-    time_calc = datetime.now().strftime('%H:%M')
-    with open('log.xml', 'a') as log_file:
-        log_file.write('{}; {}; {}; {}; \n Time: {}'.format(surname, name, tel_number, params, time_calc))
+    time_calc = datetime.datetime.now()
+    with open('log_tel.xml', 'a') as log_file:
+        log_file.write('{}; {}; {}; {}; Time: {}\n'.format(surname, name, tel_number, params, time_calc))
