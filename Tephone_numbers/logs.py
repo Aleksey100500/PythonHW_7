db7a9ec636c80f01689bac39906befaf8a6e2b49
Aleksey <a_zhuravlyov@bk.ru>
2022-10-20 11:43:12 +0300

    Added logs for CSV,JSON,XML

---
+++ Tephone_numbers/logs.py
@@ -0,0 +1,16 @@
+import datetime as datetime
+
+def logs_csv(surname, name, tel_number, params):
+    time_calc = datetime.now().strftime('%H:%M')
+    with open('log.csv', 'a') as log_file:
+        log_file.write('{}; {}; {}; {}; \n Time: {}'.format(surname, name, tel_number, params, time_calc))
+
+def logs_json(surname, name, tel_number, params):
+    time_calc = datetime.now().strftime('%H:%M')
+    with open('log.json', 'a') as log_file:
+        log_file.write('{}; {}; {}; {}; \n Time: {}'.format(surname, name, tel_number, params, time_calc))
+
+def logs_xml(surname, name, tel_number, params):
+    time_calc = datetime.now().strftime('%H:%M')
+    with open('log.xml', 'a') as log_file:
+        log_file.write('{}; {}; {}; {}; \n Time: {}'.format(surname, name, tel_number, params, time_calc))
